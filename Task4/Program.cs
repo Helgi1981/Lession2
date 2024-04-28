524d8e35e4227227dc595e9ceaf8389057da6fbd
Oleg Karlin <o.karlin1981@gmail.com>
2024-04-28 15:37:06 +0300

    add Lession2:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача №3. Гири
+// Дано N гирь с разным весом в случайном порядке. Определить вес самой тяжелой гири.
+
+// Вариант №1:
+
+// int n = 5;
+// int[] array = { 2, 4, 7, 3, 9 };
+// int i = 0;
+// int max = array[0];
+
+// while (i < array.Length)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+// Вариант №2:
+
+// int[] array = { 2, 4, 7, 3, 9 };
+// int max = array[0];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+// }
+// Console.WriteLine(max);
+
+// Вариант №3:
+
+int[] array = { 2, 4, 7, 3, 9 };
+int max = array[0];
+
+foreach (int i in array)
+{
+    if (i > max)
+    {
+        max = i;
+    }
+}
+Console.WriteLine(max);
